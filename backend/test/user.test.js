809795bc91baedb472b399e3d4a9a7ef3cb83fbe
AndersonAlpin <andersonalpin@hotmail.com>
2021-05-29 14:49:32 -0300

    Teste - Cadastro de usuário

---
+++ backend/test/user.test.js
@@ -0,0 +1,22 @@
+const app = require('../src/app')
+const supertest = require('supertest');
+const request = supertest(app);
+
+describe('Cadastro de usuário', () => {
+  test('Deve cadastrar um usuário com sucesso', () => {
+    let time = new Date();
+    let email = `${time}@email.com`
+    let user = { name: "Anderson", email, password: "12345" }
+
+    return request
+      .post("/user")
+      .send(user)
+      .then(res => {
+        expect(res.statusCode).toEqual(200);
+        expect(res.body.email).toEqual(email);
+      })
+      .catch(err => {
+        fail(err);
+      });
+  })
+});
