e9898ee437be584bc32cce3a14dd93e7f19904b4
AndersonAlpin <andersonalpin@hotmail.com>
2021-05-29 14:29:55 -0300

    Conexão com o banco

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+mongoose.connect("mongodb://localhost:27017/guiapics", { 
+  useNewUrlParser: true, useUnifiedTopology: true 
+}).then(() => {
+  console.log("Conectado com o banco!");
+}).catch(err => {
+  console.log(err);
+});
 
 app.get('/', (req, res) => {
   res.json({})
