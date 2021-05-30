0574ce951cb48a948184820c760e6c33c32eeb85
AndersonAlpin <andersonalpin@hotmail.com>
2021-05-30 12:59:08 -0300

    Nova estrutura de pastas

--- backend/src/server.js
+++
@@ -1,5 +0,0 @@
-let app = require('./app');
-
-app.listen(8080, () => {
-  console.log("Servidor rodando!");
-})
--- backend/test/server.test.js
+++ backend/test/server.test.js
@@ -1,8 +1,8 @@
-const app = require('../src/app')
+const app = require('../index')
 const supertest = require('supertest');
 const request = supertest(app);
 
-test("A aplicação deve responder na porta 8080", () => {
+test("A aplicação deve responder na porta 8686", () => {
   return request.get('/').then(res => {
     let status = res.statusCode;
     expect(status).toEqual(200);
