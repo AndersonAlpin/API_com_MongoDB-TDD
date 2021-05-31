adeb9b66250c5aeb9762afce8c5ac88361476b73
AndersonAlpin <andersonalpin@hotmail.com>
2021-05-31 00:39:25 -0300

    Validação de email

--- backend/src/controllers/UserController.js
+++ backend/src/controllers/UserController.js
@@ -4,20 +4,28 @@ class UserController {
 
     async create(req, res) {
 
-        if(req.body.name == "" || req.body.email == "" || req.body.password == ""){
+        if (req.body.name == "" || req.body.email == "" || req.body.password == "") {
             res.sendStatus(400);
             return;
         }
 
         try {
+            let user = await User.findOne({ "email": req.body.email });
+
+            if (user != undefined) {
+                res.statusCode = 400;
+                res.json({ error: "Email já cadastrado." })
+                return;
+            }
+
             let newUser = new User({
                 name: req.body.name,
                 email: req.body.email,
                 password: req.body.password
-            })
+            });
 
             await newUser.save();
-            res.json({ email: req.body.email })
+            res.json({ email: req.body.email });
         } catch (err) {
             res.sendStatus(500);
         }
