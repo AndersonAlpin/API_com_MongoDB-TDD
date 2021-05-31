439888a98373ea685b68a596499f0aa90c3047f1
AndersonAlpin <andersonalpin@hotmail.com>
2021-05-31 00:18:43 -0300

    Teste de email repetido

--- backend/test/user.test.js
+++ backend/test/user.test.js
@@ -24,5 +24,26 @@ describe('Cadastro de usuário', () => {
       .then(res => {
         expect(res.statusCode).toEqual(400); //400 = Bad Request
       });
-  })
+  });
+
+  test("Deve impedir que um usuário se cadastre com um email repetido", () => {
+    let time = new Date();
+    let email = `${time}@email.com`;
+    let user = { name: "Anderson", email, password: "12345" };
+
+    return request.post("/user")
+      .send(user)
+      .then(res => {
+        expect(res.statusCode).toEqual(200);
+        expect(res.body.email).toEqual(email);
+
+        return request.post('/user')
+          .send(user)
+          .then(res => {
+            expect(res.statusCode).toEqual(400);
+            expect(res.body.error).toEqual("Email já cadastrado.");
+          });
+
+      });
+  });
 });
