dd93a7e3e1aac60060415c9b306192283e7ad51a
AndersonAlpin <andersonalpin@hotmail.com>
2021-06-03 14:29:42 -0300

    Geração de token

--- api/src/controllers/UserController.js
+++ api/src/controllers/UserController.js
@@ -1,46 +1,59 @@
 const User = require('../models/User');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const JWTSecret = "sadcl,0fa9e&*iur#09ajiu$89aju@sdf89i"
 
 class UserController {
 
-    async create(req, res) {
+	async create(req, res) {
 
-        if (req.body.name == "" || req.body.email == "" || req.body.password == "") {
-            res.sendStatus(400);
-            return;
-        }
+		if (req.body.name == "" || req.body.email == "" || req.body.password == "") {
+			res.sendStatus(400);
+			return;
+		}
 
-        try {
-            let user = await User.findOne({ "email": req.body.email });
+		try {
+			let user = await User.findOne({ "email": req.body.email });
 
-            if (user != undefined) {
-                res.statusCode = 400;
-                res.json({ error: "Email já cadastrado." })
-                return;
-            }
+			if (user != undefined) {
+				res.statusCode = 400;
+				res.json({ error: "Email já cadastrado." })
+				return;
+			}
 
-            let password = req.body.password;
-            let salt = await bcrypt.genSalt(10);
-            let hash = await bcrypt.hash(password, salt);
+			let password = req.body.password;
+			let salt = await bcrypt.genSalt(10);
+			let hash = await bcrypt.hash(password, salt);
 
-            let newUser = new User({
-                name: req.body.name,
-                email: req.body.email,
-                password: hash
-            });
+			let newUser = new User({
+				name: req.body.name,
+				email: req.body.email,
+				password: hash
+			});
 
-            await newUser.save();
-            res.json({ email: req.body.email });
-        } catch (err) {
-            res.sendStatus(500);
-        }
-    }
+			await newUser.save();
+			res.json({ email: req.body.email });
+		} catch (err) {
+			res.sendStatus(500);
+		}
+	}
 
-    async delete(req, res) {
-        await User.deleteOne({ "email": req.params.email });
-        res.sendStatus(200);
-    }
+	async auth(req, res) {
+		let { email, password } = req.body;
+		jwt.sign({ email }, JWTSecret, { expiresIn: '48h' }, (err, token) => {
+			if (err) {
+				res.sendStatus(500);
+				console.log(err);
+			} else {
+				res.json({ token })
+			}
+		})
+	}
+
+	async delete(req, res) {
+		await User.deleteOne({ "email": req.params.email });
+		res.sendStatus(200);
+	}
 }
 
 module.exports = new UserController();
--- api/src/routes/routes.js
+++ api/src/routes/routes.js
@@ -1,8 +1,9 @@
-var express = require("express")
+var express = require("express");
 var router = express.Router();
 var UserController = require("../controllers/UserController");
 
 router.post('/user', UserController.create);
-router.delete('/user/:email', UserController.delete)
+router.delete('/user/:email', UserController.delete);
+router.post('/auth', UserController.auth);
 
 module.exports = router;
