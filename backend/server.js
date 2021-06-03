4cb714f1b9f78ac305534e053e3d85c4d7b0b8f7
AndersonAlpin <andersonalpin@hotmail.com>
2021-06-03 15:11:26 -0300

    API concluida

--- backend/server.js
+++
@@ -1,5 +0,0 @@
-let app = require('.')
-
-app.listen(8686, () => {
-  console.log("Servidor rodando!");
-})
--- backend/src/routes/routes.js
+++
@@ -1,7 +0,0 @@
-var express = require("express")
-var router = express.Router();
-var UserController = require("../controllers/UserController");
-
-router.post('/user', UserController.create);
-
-module.exports = router;
